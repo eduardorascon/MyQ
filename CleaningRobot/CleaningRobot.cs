5c53202b176c01d97d851cb24eeeccfc87e1191d
Luis Eduardo Rascon Fierro <luis.rascon@gmail.com>
2018-05-29 19:09:46 -0500

    First version, still missing implementation for the following methods:     Clean, GoForward and GoBack.

---
+++ CleaningRobot/CleaningRobot.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using CleaningRobot.BasicInstructions;
+
+namespace CleaningRobot
+{
+
+    public class CleaningRobot
+    {
+        protected int Battery { get; set; }
+        protected int PositionX { get; set; }
+        protected int PositionY { get; set; }
+        protected String FacingTo { get; set; }
+
+        public CleaningRobot(int battery, int positionX, int positionY, String facing)
+        {
+            this.Battery = battery;
+            this.PositionX = positionX;
+            this.PositionY = positionY;
+            this.FacingTo = facing;
+        }
+
+        public void ExecuteInstructions(List<IBasicInstruction> instruccionsList, String[,] map)
+        {
+            foreach (IBasicInstruction instruction in instruccionsList)
+            {
+                if (this.Battery - instruction.EnergyConsumtion < 0)
+                    break;
+
+                this.Battery -= instruction.EnergyConsumtion;
+
+                switch (instruction.InstrucctionName)
+                {
+                    case "TL":
+                        TurnLeft();
+                        break;
+                    case "TR":
+                        TurnRight();
+                        break;
+                    case "A":
+                        GoForward();
+                        break;
+                    case "B":
+                        GoBack();
+                        break;
+                    case "C":
+                        Clean();
+                        break;
+                }
+            }
+        }
+
+        //TODO
+        private void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO
+        private void GoForward()
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO
+        private void Clean()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TurnLeft()
+        {
+            switch (this.FacingTo)
+            {
+                case "N":
+                    this.FacingTo = "W";
+                    break;
+                case "S":
+                    this.FacingTo = "E";
+                    break;
+                case "E":
+                    this.FacingTo = "N";
+                    break;
+                case "W":
+                    this.FacingTo = "S";
+                    break;
+            }
+        }
+        private void TurnRight()
+        {
+            switch (this.FacingTo)
+            {
+                case "N":
+                    this.FacingTo = "E";
+                    break;
+                case "S":
+                    this.FacingTo = "W";
+                    break;
+                case "E":
+                    this.FacingTo = "S";
+                    break;
+                case "W":
+                    this.FacingTo = "N";
+                    break;
+            }
+        }
+    }
+}
--- CleaningRobot/Program.cs
+++ CleaningRobot/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CleaningRobot.BasicInstructions;
 
 namespace CleaningRobot
 {
@@ -10,6 +8,36 @@ namespace CleaningRobot
     {
         static void Main(string[] args)
         {
+            CleaningRobot bot = new CleaningRobot(80, 1, 0, "N");
+            List<IBasicInstruction> commandList = InstructionsHelper.ConvertToBasicInstrucctions(new string[] { "TL", "A", "C", "A", "C", "TR", "A", "C" });
+            String[,] map = new string[4, 4] {
+                { "S", "S", "S", "S" },
+                { "S", "S", "C", "S" },
+                { "S", "S", "S", "S" },
+                { "S", "null", "S", "S" }
+            };
+
+            Simulation simulation = new Simulation(bot, commandList, map);
+            simulation.Run();
+        }
+    }
+
+    public class Simulation
+    {
+        private CleaningRobot bot;
+        private List<IBasicInstruction> instructionsList;
+        private String[,] map;
+        public Simulation(CleaningRobot cleaningRobot, List<IBasicInstruction> commandList, String[,] map)
+        {
+            this.instructionsList = commandList;
+            this.bot = cleaningRobot;
+            this.map = map;
+        }
+
+        public void Run()
+        {
+            bot.ExecuteInstructions(instructionsList, map);
+
         }
     }
 }
